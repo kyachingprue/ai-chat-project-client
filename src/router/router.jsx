c8bb57626446f921363b4dd71ca91f3769883968
kyachingprue <kyachingpruemarma.info@gmail.com>
2026-05-02 10:26:17 +0600

    new code updated

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layout/MainLayout";
-import Error from "../pages/Error";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Services from "../pages/Services";
-import Contact from "../pages/Contact";
-import Login from "../components/Authentication/Login/Login";
-import Register from "../components/Authentication/Register/Register";
-import Profile from "../pages/Profile";
+import { createBrowserRouter } from 'react-router-dom'
+import MainLayout from '../layout/MainLayout'
+import Error from '../pages/Error'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Services from '../pages/Services'
+import Contact from '../pages/Contact'
+import Login from '../components/Authentication/Login/Login'
+import Register from '../components/Authentication/Register/Register'
+import Profile from '../pages/Profile'
 
 const router = createBrowserRouter([
   {
@@ -16,35 +16,35 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: '/',
-        element:<Home/>
+        index: true,
+        element: <Home />
       },
       {
         path: 'about',
-        element: <About/>
+        element: <About />
       },
       {
         path: 'services',
-        element: <Services/>
+        element: <Services />
       },
       {
         path: 'contact',
-        element: <Contact/>
+        element: <Contact />
       },
       {
         path: 'profile',
-        element: <Profile/>
+        element: <Profile />
       }
     ]
   },
   {
     path: '/login',
-    element: <Login/>
+    element: <Login />
   },
   {
     path: '/register',
-    element: <Register/>
+    element: <Register />
   }
 ])
 
-export default router;
+export default router
