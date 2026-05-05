6dc46235f296e40b23f660063bc395a00898cad1
kyachingprue <kyachingpruemarma.info@gmail.com>
2026-05-05 12:19:28 +0600

    new cursor pointer animation add

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/router.jsx'
 import { Toaster } from 'react-hot-toast'
 import AuthProvider from './provider/AuthProvider.jsx'
+import './index.css'
+import SplashCursor from './components/SplashCursor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+      <SplashCursor/>
       <RouterProvider router={router} />
       <Toaster position="top-right" reverseOrder={false} />
     </AuthProvider>
